cda06e2c15321b2c4585238c6739c9a8e2a6c37d
Kevin rojas <kevinroj326@gmail.com>
2023-09-14 18:03:59 -0500

    acomodando secciones

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,13 +2,16 @@ import React from "react";
 
 const Article = () => {
   return (
-    <div className="mt-[40%] p-6 ">
-        <h2 className="text-4xl text-center mb-12">Latest Articles</h2>
-      <div className="bg-GrayishBlue rounded-md">
-        <div className="">
-          <img className="rounded-md" src="./public/images/image-currency.jpg" alt="" />
-        </div>
-        <h4>By Claire Robinson</h4>
+    <div className="mt-[20%] p-6 flex flex-col gap-8">
+      <h2 className="text-4xl text-center mb-12">Latest Articles</h2>
+      <div className="text-GrayishBlue">
+        <img
+          className="rounded-t-lg"
+          src="./public/images/image-currency.jpg"
+          alt=""
+        />
+
+        <h4 className="p-4 text-[12px]">By Claire Robinson</h4>
         <h2>Receive money in any currency with no fees</h2>
         <p>
           The world is getting smaller and we're becoming more mobile. So why
@@ -16,11 +19,13 @@ const Article = () => {
         </p>
       </div>
 
-      <div>
-        <div>
-          <img src="./public/images/image-restaurant.jpg" alt="" />
-        </div>
-        <h4>By Wilson Hutton</h4>
+      <div className="text-GrayishBlue">
+        <img
+          className="rounded-t-lg"
+          src="./public/images/image-restaurant.jpg"
+          alt=""
+        />
+        <h4 className="p-4 text-[12px]">By Wilson Hutton</h4>
         <h2>Treat yourself without worrying abour money</h2>
         <p>
           Our simple budgeting teature allows you to separate out your spending
@@ -28,26 +33,33 @@ const Article = () => {
         </p>
       </div>
 
-      <div>
-        <div>
-          <img src="./public/images/image-plane.jpg" alt="" />
-        </div>
-        <h4>By Wilson Hutton</h4>
+      <div className="text-GrayishBlue">
+        <img
+          className="rounded-t-lg"
+          src="./public/images/image-plane.jpg"
+          alt=""
+        />
+
+        <h4 className="p-4 text-[12px]">By Wilson Hutton</h4>
         <h2>Take your Easybank card wherever you go</h2>
         <p>
-         We want you to enjoy your travels. This is why we don't change any fees on purchases while you're abroad. We'll even show you ...
+          We want you to enjoy your travels. This is why we don't change any
+          fees on purchases while you're abroad. We'll even show you ...
         </p>
       </div>
 
-      <div>
+      <div className="text-GrayishBlue">
+        <img
+          className="rounded-t-lg"
+          src="./public/images/image-confetti.jpg"
+          alt=""
+        />
 
-        <div>
-          <img src="./public/images/image-confetti.jpg" alt="" />
-        </div>
-        <h4>By Claire Robinson</h4>
+        <h4 className="p-4 text-[12px]">By Claire Robinson</h4>
         <h2>Our invite-only Beta accounts are now live!</h2>
         <p>
-          After a lot of hard work by the whole team, we're excited to launch our closed beta. It's easy to request an invite throung the site..
+          After a lot of hard work by the whole team, we're excited to launch
+          our closed beta. It's easy to request an invite throung the site..
         </p>
       </div>
     </div>
--- src/components/ChooseBnk.jsx
+++ src/components/ChooseBnk.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ChooseBnk = () => {
   return (
-    <div className="bg-red-500 mt-[20%]">
+    <div className="mt-24">
       <div className="p-2 text-center">
         <h2 className="text-4xl">Why choose</h2>
         <span className="text-4xl">Easybank?</span>
@@ -16,7 +16,7 @@ const ChooseBnk = () => {
       <div className="bg-LightGrayish p-8 lg:flex text-center">
         <div className="mt-12">
           <div className="flex items-center justify-center">
-            <img src="/images/icon-online.svg" alt="" />
+            <img src="./images/icon-online.svg" alt="" />
           </div>
           <h2 className="text-xl mt-4">Online Banking</h2>
           <p className="mt-4 text-sm px-14">
@@ -27,7 +27,7 @@ const ChooseBnk = () => {
 
         <div className="mt-12">
           <div className="flex items-center justify-center">
-            <img src="/images/icon-onboarding.svg" alt="" />
+            <img src="./images/icon-onboarding.svg" alt="" />
           </div>
           <h2 className="text-xl mt-4">Fast Onboarding</h2>
           <p className="mt-4 text-sm px-12">
@@ -38,7 +38,7 @@ const ChooseBnk = () => {
 
         <div className="mt-12">
           <div className="flex items-center justify-center">
-            <img src="/images/icon-api.svg" alt="" />
+            <img src="./images/icon-api.svg" alt="" />
           </div>
           <h2 className="text-xl mt-4">Open API</h2>
           <p className="mt-4 text-sm px-14">
@@ -49,7 +49,7 @@ const ChooseBnk = () => {
 
         <div className="mt-12">
           <div className="flex items-center justify-center">
-            <img src="/images/icon-api.svg" alt="" />
+            <img src="./images/icon-api.svg" alt="" />
           </div>
           <h2 className="text-xl mt-4">Open API</h2>
           <p className="mt-4 text-sm px-14">
