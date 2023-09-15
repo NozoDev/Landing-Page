534f87ebfc2b2237da22ba03adf5268092b957ae
Kevin rojas <kevinroj326@gmail.com>
2023-09-15 15:49:55 -0500

    corecciones

--- src/components/HeaderMain.jsx
+++ src/components/HeaderMain.jsx
@@ -5,7 +5,7 @@ const HeaderMain = () => {
     <main className="grid md:grid-cols-2 max-w-[1200px] mx-auto md:h-[400px] ">
       {/* Columna izquierda para el texto en dispositivos grandes */}
       <div className="order-2 md:order-1 flex justify-center items-center text-center ">
-        <div className="md:first-letter:py-4 md:flex md:flex-col md:items-start  ">
+        <div className="md:first-letter:py-4 md:text-left md:flex md:flex-col md:items-start  ">
           <h2 className="text-4xl my-0  tracking-wide font-semibold text-black">
             Next generation
           </h2>
@@ -36,10 +36,10 @@ const HeaderMain = () => {
 
       {/* Columna derecha para las imágenes en dispositivos grandes */}
       <div className="order-1 md:order-2 md:flex md:flex-col md:items-start">
-        <div className="h-[420px] w-auto">
+        <div className="h-[420px]">
           {/* Imagen de desktop */}
           <img
-            className="md:block hidden md:w-full md:h-[470px] lg:h-[650px]"
+            className="md:block hidden md:w-full md:h-[470px] lg:h-[650px] xl:left-3/4"
             src="./images/bg-intro-desktop.svg"
             alt=""
           />
@@ -55,7 +55,7 @@ const HeaderMain = () => {
             {/* Otras imágenes de desktop */}
             <div className="flex justify-center items-center">
               <img
-                className="absolute top-0 w-[350px] h-[420px] md:w-[390px] md:h-[590px] lg:w-[460px] lg:h-[780px]"
+                className="absolute top-0 w-[350px] h-[420px] md:w-[390px] md:h-[590px] lg:w-[460px] lg:h-[780px] xl:-top-20 xl:w-[550px] xl:left-[60%]"
                 src="./images/image-mockups.png"
                 alt=""
               />
