960797f2677dd806e39054663a7f3248fa32ab63
Kevin rojas <kevinroj326@gmail.com>
2023-09-16 15:00:36 -0500

    colocando tamaños a los textos

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -15,10 +15,10 @@ const Article = () => {
           <h4 className="p-4 text-[10px] lg:-ml-28 hover:text-blue-500 cursor-pointer">
             By Claire Robinson
           </h4>
-          <h2 className="text-[10px] p-2 ">
+          <h2 className="text-[10px] lg:text-[15px] xl:text-[15px] md:text-[15px] p-2 ">
             Receive money in any currency with no fees
           </h2>
-          <p className="text-[10px] p-2">
+          <p className="text-[10px] lg:text-[15px] xl:text-[15px] md:text-[15px] p-2">
             The world is getting smaller and we're becoming more mobile. So why
             should you be forced to only receive money in a single...
           </p>
@@ -33,10 +33,10 @@ const Article = () => {
           <h4 className="p-4 text-[10px] lg:-ml-28 hover:text-blue-500 cursor-pointer">
             By Wilson Hutton
           </h4>
-          <h2 className="text-[10px] p-2 ">
+          <h2 className="text-[10px] lg:text-[15px] xl:text-[15px] md:text-[15px] p-2 ">
             Treat yourself without worrying abour money
           </h2>
-          <p className="text-[10px] p-2">
+          <p className="text-[10px] lg:text-[15px] xl:text-[15px] md:text-[15px] p-2 ">
             Our simple budgeting teature allows you to separate out your
             spending and set realistic limits each month. That means you
           </p>
@@ -52,10 +52,10 @@ const Article = () => {
           <h4 className="p-4 text-[10px] lg:-ml-28 hover:text-blue-500 cursor-pointer">
             By Wilson Hutton
           </h4>
-          <h2 className="text-[10px] p-2 ">
+          <h2 className="text-[10px] lg:text-[15px] xl:text-[15px] md:text-[15px] p-2 ">
             Take your Easybank card wherever you go
           </h2>
-          <p className="text-[10px] p-2">
+          <p className="text-[10px] lg:text-[15px] xl:text-[15px] md:text-[15px] p-2">
             We want you to enjoy your travels. This is why we don't change any
             fees on purchases while you're abroad. We'll even show you ...
           </p>
@@ -71,10 +71,10 @@ const Article = () => {
           <h4 className="p-4 text-[10px] lg:-ml-28  hover:text-blue-500 cursor-pointer">
             By Claire Robinson
           </h4>
-          <h2 className="text-[10px]  p-2">
+          <h2 className="text-[10px] lg:text-[15px] xl:text-[15px] md:text-[15px] p-2">
             Our invite-only Beta accounts are now live!
           </h2>
-          <p className="text-[10px] p-2">
+          <p className="text-[10px] lg:text-[15px] xl:text-[15px] md:text-[15px] p-2">
             After a lot of hard work by the whole team, we're excited to launch
             our closed beta. It's easy to request an invite throung the site..
           </p>
