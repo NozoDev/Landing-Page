c86394f5d20bc87d7d8108d7bb711f675b9f9324
Kevin rojas <kevinroj326@gmail.com>
2023-09-16 14:36:47 -0500

    :D casi listo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,35 +3,39 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-DarkBlue  h-[450px]">
-      <div className="flex items-center px-2 mt-10  justify-center ">
+      <div className="flex bg-white animate-pulse items-center px-2 mt-10  justify-center ">
         <img className="" src="/images/logo.svg"></img>
       </div>
 
-      <div className="flex justify-center gap-2 mt-10">
-        <div>
-          <img src="/images/icon-facebook.svg" alt="" />
+      <div className="flex justify-center gap-4 mt-10">
+        <div className="hover:animate-pulse cursor-pointer">
+          <a href="www.facebook.com" target="_blank">
+            <img className="w-8" src="/images/icon-facebook.svg" alt="facebook.com" />
+          </a>
         </div>
         <div>
-          <img src="/images/icon-youtube.svg" alt="" />
+          <a href="www.youtube.com" target="_blank">
+            <img className="w-8" src="/images/icon-youtube.svg" alt="youtube" />
+          </a>
         </div>
         <div>
-          <img src="/images/icon-twitter.svg" alt="" />
+         <a href="www.twitter.com" target="_blank"> <img className="w-8" src="/images/icon-twitter.svg" alt="twitter" /></a>
         </div>
         <div>
-          <img src="/images/icon-pinterest.svg" alt="" />
+          <a href="www.pinterest.com" target="_blank"><img className="w-8" src="/images/icon-pinterest.svg" alt="pinterest" /></a>
         </div>
         <div>
-          <img src="/images/icon-instagram.svg" alt="" />
+         <a href="www.instagram.com" target="_blank"> <img className="w-8" src="/images/icon-instagram.svg" alt="instagram" /></a>
         </div>
       </div>
 
       <div className="flex flex-col justify-center items-center gap-4 mt-4">
-        <a href="">About Us</a>
-        <a href="">Contact</a>
-        <a href="">Blog</a>
-        <a href="">Careers</a>
-        <a href="">Support</a>
-        <a href="">Privacy Policy</a>
+        <a className="hover:text-2xl font-bold" href="/">About Us</a>
+        <a className="hover:text-2xl font-bold" href="#">Contact</a>
+        <a  className="hover:text-2xl font-bold"href="#">Blog</a>
+        <a  className="hover:text-2xl font-bold"href="#">Careers</a>
+        <a  className="hover:text-2xl font-bold"href="#">Support</a>
+        <a className="hover:text-2xl font-bold" href="#">Privacy Policy</a>
 
         <button
           type="submit"
