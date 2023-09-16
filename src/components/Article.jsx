9a92b7e02514b834d9d0af2ef9c0b62a0e68b5ac
Kevin rojas <kevinroj326@gmail.com>
2023-09-15 19:18:25 -0500

    acomodadndo los alturas y anchuras de ls imagenes a ver cual queda mejor

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,63 +2,71 @@ import React from "react";
 
 const Article = () => {
   return (
-    <div className="mt-[20%] p-6">
+    <div className="mt-[20%] p-6 flex flex-col">
       <h2 className="text-4xl text-center mb-12">Latest Articles</h2>
-      <div className="gap-4 flex flex-col xl:flex  ">
-        <div className="text-GrayishBlue">
+      <div className="gap-4 lg:flex ">
+        <div className="text-GrayishBlue lg:flex lg:justify-center items-center lg:flex-col ">
           <img
-            className="rounded-t-lg w-full"
+            className="rounded-t-lg lg:w-full md:w-full lg:h-[175px]"
             src="./images/image-currency.jpg"
             alt="currency"
           />
 
           <h4 className="p-4 text-[12px]">By Claire Robinson</h4>
-          <h2>Receive money in any currency with no fees</h2>
-          <p>
+          <h2 className="text-[10px]">
+            Receive money in any currency with no fees
+          </h2>
+          <p className="text-[10px]">
             The world is getting smaller and we're becoming more mobile. So why
             should you be forced to only receive money in a single...
           </p>
         </div>
 
-        <div className="text-GrayishBlue ">
+        <div className="text-GrayishBlue lg:flex lg:justify-center items-center lg:flex-col">
           <img
-            className="rounded-t-lg w-full"
+            className="rounded-t-lg lg:w-full md:w-full"
             src="./images/image-restaurant.jpg"
             alt="restaurant"
           />
           <h4 className="p-4 text-[12px]">By Wilson Hutton</h4>
-          <h2>Treat yourself without worrying abour money</h2>
-          <p>
+          <h2 className="text-[10px]">
+            Treat yourself without worrying abour money
+          </h2>
+          <p className="text-[10px]">
             Our simple budgeting teature allows you to separate out your
             spending and set realistic limits each month. That means you
           </p>
         </div>
 
-        <div className="text-GrayishBlue">
+        <div className="text-GrayishBlue lg:flex lg:justify-center items-center lg:flex-col">
           <img
-            className="rounded-t-lg w-full"
+            className="rounded-t-lg lg:w-full md:w-full"
             src="./images/image-plane.jpg"
             alt="plane"
           />
 
           <h4 className="p-4 text-[12px]">By Wilson Hutton</h4>
-          <h2>Take your Easybank card wherever you go</h2>
-          <p>
+          <h2 className="text-[10px]">
+            Take your Easybank card wherever you go
+          </h2>
+          <p className="text-[10px]">
             We want you to enjoy your travels. This is why we don't change any
             fees on purchases while you're abroad. We'll even show you ...
           </p>
         </div>
 
-        <div className="text-GrayishBlue">
+        <div className="text-GrayishBlue lg:flex lg:justify-center items-center lg:flex-col">
           <img
-            className="rounded-t-lg w-full"
+            className="rounded-t-lg lg:w-full md:w-full"
             src="./images/image-confetti.jpg"
             alt="confetti"
           />
 
-          <h4 className="p-4 text-[12px]">By Claire Robinson</h4>
-          <h2>Our invite-only Beta accounts are now live!</h2>
-          <p>
+          <h4 className="p-4 text-[12px] ">By Claire Robinson</h4>
+          <h2 className="text-[10px]">
+            Our invite-only Beta accounts are now live!
+          </h2>
+          <p className="text-[10px]">
             After a lot of hard work by the whole team, we're excited to launch
             our closed beta. It's easy to request an invite throung the site..
           </p>
